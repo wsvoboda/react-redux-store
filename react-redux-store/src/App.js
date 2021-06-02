5c0d3ea4c36e43ac0067e8747300efe9e50bcd53
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-02 10:59:34 -0500

    added react router and links to pages

--- react-redux-store/src/App.js
+++ react-redux-store/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>React Redux Store</h1>
     </div>
   );
--- react-redux-store/src/components/Navbar.js
+++ react-redux-store/src/components/Navbar.js
@@ -1,5 +1,43 @@
 import React from "react";
+import "../App.css";
+
+import AboutUs from "./AboutUs";
+import Products from "./Products";
+import Cart from "./Cart";
+import Landing from "./Landing";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/aboutus">About Us</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/cart">Cart</Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/aboutus">
+            <AboutUs />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="*">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
