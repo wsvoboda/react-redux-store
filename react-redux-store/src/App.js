3d71eeaa5f881152f2df0218e99664d1c809ba31
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-02 11:43:02 -0500

    create and style contents of footer

--- react-redux-store/src/App.js
+++ react-redux-store/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <h1>React Redux Store</h1>
+      <Footer />
     </div>
   );
 }
--- react-redux-store/src/components/Footer.js
+++ react-redux-store/src/components/Footer.js
@@ -1,5 +1,22 @@
 import React from "react";
+import "../App.css";
+
+import fb from "../assets/fb.png";
+import insta from "../assets/insta.png";
+import twitter from "../assets/twitter.png";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer-links">
+      <a href="https://facebook.com">
+        <img src={fb} alt="facebook" />
+      </a>
+      <a href="https://instagram.com">
+        <img src={insta} alt="instagram" />
+      </a>
+      <a href="https://twitter.com">
+        <img src={twitter} alt="twitter" />
+      </a>
+    </div>
+  );
 }
