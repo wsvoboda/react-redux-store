87a61e064c20bacd9de43f92eb5e221084dcbec0
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-03 15:48:37 -0500

    added cart mapping

--- react-redux-store/src/components/Cart.js
+++ react-redux-store/src/components/Cart.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import "../App.css";
 
 export default function Cart() {
+  const cart = useSelector((state) => state.cart);
   return (
     <div>
       <h1>Cart</h1>
+      {cart.map((items) => {
+        return (
+          <div className="cart-items">
+            <img src={items.image} alt="cart-item" />
+            <p>{items.title}</p>
+            <p>${items.price}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
