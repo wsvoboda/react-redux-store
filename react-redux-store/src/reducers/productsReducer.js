2297e18c6ce78a9d493dcf5a2d412b30848373e0
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-03 14:24:09 -0500

    commented out broken code

--- react-redux-store/src/reducers/productsReducer.js
+++ react-redux-store/src/reducers/productsReducer.js
@@ -1,11 +1,20 @@
 import { FILL_PRODUCTS_FROM_API } from "../action-types/products-action-types";
+// import { FILL_INDIVIDUAL_PRODUCT } from "../action-types/products-action-types";
 
-const initialState = [{}];
+const initialState = /*allProducts: [{}], oneProduct:*/ [{}];
 
 const products = (state = initialState, action) => {
   switch (action.type) {
+    // case FILL_PRODUCTS_FROM_API:
+    //   return { ...state, allProducts: [action.payload] };
+    // case FILL_INDIVIDUAL_PRODUCT:
+    //   return { ...state, oneProduct: [action.payload] };
+    // default:
+    //   return state;
     case FILL_PRODUCTS_FROM_API:
       return (state = action.payload);
+    // case FILL_INDIVIDUAL_PRODUCT:
+    //   return (state = action.payload);
     default:
       return state;
   }
