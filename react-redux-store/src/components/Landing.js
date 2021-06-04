37fd1aa0242918668111ee26c3facd21bb170856
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-04 10:36:38 -0500

    added styling to front page and buttons

--- react-redux-store/src/components/Landing.js
+++ react-redux-store/src/components/Landing.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bag from "../assets/bag.jpg";
 import "../App.css";
 
 export default function Landing() {
   return (
     <div className="Landing">
       <h1>Welcome to Odds & Ends!</h1>
-      <h3>Estabilished in 2021, we provide a source of all random things</h3>
-      <h3>Have a look!</h3>
-      <Link to="/products/">
+      <h3>Estabilished in 2021, we provide a source of all random things.</h3>
+      <h3>Take a look!</h3>
+      <Link id="button-link" to="/products/">
         <button>Products</button>
       </Link>
+      <img id="bag" src={bag} alt="bag" />
     </div>
   );
 }
