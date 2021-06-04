bc358bd0132c9ed859dcbcb705c910224d9e75aa
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-04 09:57:26 -0500

    calculating grand total

--- react-redux-store/src/components/Cart.js
+++ react-redux-store/src/components/Cart.js
@@ -4,6 +4,12 @@ import "../App.css";
 
 export default function Cart() {
   const cart = useSelector((state) => state.cart);
+  let grandTotalArray = [];
+  cart.map((prices) => grandTotalArray.push(prices.price));
+  let grandTotal = 0;
+  for (let i = 0; i < grandTotalArray.length; i++) {
+    grandTotal += grandTotalArray[i];
+  }
   return (
     <div className="Cart">
       <h1>Cart</h1>
@@ -11,11 +17,14 @@ export default function Cart() {
         return (
           <div className="cart-items">
             <img src={items.image} alt="cart-item" />
-            <p>{items.title}</p>
+            {/* <p>{items.title}</p> */}
             <p>${items.price}</p>
           </div>
         );
       })}
+      <p>
+        <strong>Grand Total:</strong> ${grandTotal.toFixed(2)}
+      </p>
     </div>
   );
 }
