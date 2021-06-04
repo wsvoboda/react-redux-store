c7e4ac42bc6abc099beb7d5d0c921cdf0dc3de50
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-04 10:43:48 -0500

    added image to About Us page

--- react-redux-store/src/components/AboutUs.js
+++ react-redux-store/src/components/AboutUs.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "../App.css";
+import store from "../assets/store.jpg";
 
 export default function AboutUs() {
   return (
-    <div className="About">
-      <h1>About Us</h1>
-      <h3>
-        We're the best store, ever. All items are ethically sourced and
-        organically made. We give thanks for all of our vendors. You make this
-        store what it is!
-      </h3>
+    <div>
+      <div className="About">
+        <h1>About Us</h1>
+        <h3>
+          We're the best store, ever. All items are ethically sourced and
+          organically made. We give thanks to all our vendors and customers. You
+          make this store what it is!
+        </h3>
+      </div>
+      <div className="store">
+        <img id="store" src={store} alt="store" />
+      </div>
     </div>
   );
 }
--- react-redux-store/src/components/Landing.js
+++ react-redux-store/src/components/Landing.js
@@ -7,7 +7,7 @@ export default function Landing() {
   return (
     <div className="Landing">
       <h1>Welcome to Odds & Ends!</h1>
-      <h3>Estabilished in 2021, we provide a source of all random things.</h3>
+      <h3>Estabilished in 2021, we provide a way to find all random things.</h3>
       <h3>Take a look!</h3>
       <Link id="button-link" to="/products/">
         <button>Products</button>
